a26518844a8c88a31885491a65accb1141c50182
Clément MOUCHET <clement.mouchet@gmail.com>
2015-03-11 20:57:40 +0000

    Updated Sphinx doc

---
+++ doc/_build/html/_sources/changelog.txt
@@ -0,0 +1,10 @@
+:tocdepth: 1
+
+---------
+Changelog
+---------
+
+v0.1 2014-10
+============
+
+Initial version
--- doc/conf.py
+++ doc/conf.py
@@ -26,6 +26,8 @@ import shlex
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
+primary_domain = 'js'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.

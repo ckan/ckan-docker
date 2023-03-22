e3ab3927369e86be7568447c15e9eff98647c8be
mjanez <96422458+mjanez@users.noreply.github.com>
2023-03-22 17:03:33 +0000

    Update ckanext-xloader

--- ckan/docker-entrypoint.d/setup_xloader.sh
+++ ckan/docker-entrypoint.d/setup_xloader.sh
@@ -9,3 +9,6 @@ ckan config-tool $CKAN_INI \
 # Create ckanext-xloader API_TOKEN
 echo "Set up ckanext.xloader.api_token in the CKAN config file"
 ckan config-tool $CKAN_INI "ckanext.xloader.api_token = $(ckan -c $CKAN_INI user token add ckan_admin xloader | tail -n 1 | tr -d '\t')"
+
+# Setup worker
+ckan config-tool $CKAN_INI jobs worker

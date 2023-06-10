969494b9aabadbd53b5db22d1c42536fc66f5cde
Brett <brett@kowh.ai>
2023-06-10 08:11:18 +0200

    Updates to environment variables

--- postgresql/docker-entrypoint-initdb.d/10_create_ckandb.sql
+++
@@ -1,4 +0,0 @@
-\set ckan_db_password '\'' `echo $CKAN_DB_PASSWORD` '\''
-
-CREATE ROLE ckandbuser NOSUPERUSER CREATEDB CREATEROLE LOGIN PASSWORD :ckan_db_password;
-CREATE DATABASE ckandb OWNER ckandbuser ENCODING 'utf-8';

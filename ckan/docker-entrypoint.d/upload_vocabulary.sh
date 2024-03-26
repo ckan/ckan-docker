d70d06df2f1b21f440e83c6653bc1e4b1e13fc33
Bruno Pacheco <brunopacheco1@yahoo.com>
2024-03-26 08:40:31 +0100

    chore: preload CKAN term translation

--- ckan/docker-entrypoint.d/upload_vocabulary.sh
+++ ckan/docker-entrypoint.d/upload_vocabulary.sh
@@ -14,7 +14,8 @@ psql postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:5432/$CKAN_DB
   \if :table_not_empty
       \echo 'term_translation table is not empty, skipping'
   \else
-      \copy public.term_translation FROM  '/prepopulate_db/common_vocabulary_tags.csv' WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+      \copy public.term_translation FROM  '/docker-entrypoint.d/common_vocabulary_tags.csv' WITH (DELIMITER ',', FORMAT CSV, HEADER TRUE);
+      \echo 'term_translation initialized with common_vocabulary_tags.csv'
   \endif
 
 EOSQL

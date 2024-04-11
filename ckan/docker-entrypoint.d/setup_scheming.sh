6b3900cafffdbae7e143018accf40ab4d28d64d7
anastasianayden <anastasia.nayden@health-ri.nl>
2024-04-11 11:53:36 +0200

    fix: set up profiles to fix parsing from file bug

--- ckan/docker-entrypoint.d/setup_scheming.sh
+++ ckan/docker-entrypoint.d/setup_scheming.sh
@@ -9,4 +9,6 @@ echo "[ckanext-scheming] Setting up config-options"
 ckan config-tool $CKAN_INI -s app:main \
     "scheming.dataset_schemas = ckanext.gdi_userportal:scheming/schemas/gdi_userportal.json"\
     "scheming.presets = ckanext.scheming:presets.json"\
-    "scheming.dataset_fallback = false"
+    "scheming.dataset_fallback = false"\
+    "ckanext.dcat.rdf.profiles = euro_dcat_ap_2 fairdatapoint_dcat_ap"\
+    "ckanext.dcat.compatibility_mode = true"

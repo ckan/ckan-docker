0244b2a0512997362c9b1228edb56af28d59096a
Hans-Chrstian <hanschristian.vanderwerf@health-ri.nl>
2024-09-27 11:50:35 +0200

    feat: Upgrade to DCAT AP 3 - Update dockerfiles - Set correct schema configuration

--- ckan/docker-entrypoint.d/setup_scheming.sh
+++ ckan/docker-entrypoint.d/setup_scheming.sh
@@ -7,8 +7,10 @@
 # Update the config file with each extension config-options
 echo "[ckanext-scheming] Setting up config-options"
 ckan config-tool $CKAN_INI -s app:main \
-    "scheming.dataset_schemas = ckanext.gdi_userportal:scheming/schemas/gdi_userportal.json" \
-    "scheming.presets = ckanext.scheming:presets.json ckanext.gdi_userportal:scheming/presets/gdi_presets.json" \
+    "scheming.dataset_schemas = ckanext.dcat.schemas:dcat_ap_full.yaml ckanext.gdi_userportal:scheming/schemas/gdi_userportal.yaml" \
+    "scheming.presets = ckanext.scheming:presets.json ckanext.dcat.schemas:presets.yaml ckanext.gdi_userportal:scheming/presets/gdi_presets.yaml" \
     "scheming.dataset_fallback = false" \
-    "ckanext.dcat.rdf.profiles = euro_dcat_ap_2 fairdatapoint_dcat_ap" \
+    "ckanext.dcat.rdf.profiles = euro_dcat_ap_3 euro_dcat_ap_scheming fairdatapoint_dcat_ap" \
     "ckanext.dcat.compatibility_mode = false"
+
+

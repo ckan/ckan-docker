6376984a434e9bb4ec58b6b882a728b31daafa8f
folhesgabriel <pisagabriel09@gmail.com>
2025-01-09 15:15:56 -0300

    feat: add postgits default image

--- postgresql/docker-entrypoint-initdb.d/40_setup_spatial_postgis.sh
+++ postgresql/docker-entrypoint-initdb.d/40_setup_spatial_postgis.sh
@@ -9,4 +9,3 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE EXTENSION IF NOT EXISTS postgis_topology;
 EOSQL
 
-echo "PostGIS extension initialized successfully."

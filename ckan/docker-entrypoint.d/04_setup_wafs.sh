3b8549779609e43b5650e9f6b07343f2c1348a70
bonnland <bonnland@ucar.edu>
2025-09-03 12:15:30 -0600

    Change waf-dir creation commands

--- ckan/docker-entrypoint.d/04_setup_wafs.sh
+++ ckan/docker-entrypoint.d/04_setup_wafs.sh
@@ -19,10 +19,14 @@ if [[ $CKAN__PLUGINS == *"spatial"* ]]; then
   fi
 
   # Create web-accessible folder structure
+  if [ ! -d "/var/www/html" ]; then
+      mkdir -p /var/www/html
+  fi
+
   if [ ! -d "/var/www/html/sagedev-dset-harvest-test" ]; then
-      cd /var/www/html
-      git clone https://github.com/NCAR/sagedev-dset-harvest-test.git
-      ckan -c ~/ckan.ini harvester source create "mini-waf" "http://nginx:9000/sagedev-dset-harvest-test" "waf" "MINI WAF" "TRUE" "ncar" "MANUAL" '{"user" : "ckan_admin", "read_only": true}'
+      cd /var/www/html && \
+      git clone https://github.com/NCAR/sagedev-dset-harvest-test.git && \
+      ckan -c ~/ckan.ini harvester source create "mini-waf2" "http://nginx:9000/sagedev-dset-harvest-test" "waf" "MINI WAF2" "TRUE" "ncar" "MANUAL" '{"user" : "ckan_admin", "read_only": true}'
       # We can't run this command right away, because nginx has not started up yet.
       #ckan -c ~/ckan.ini harvester run-test mini-waf
   fi

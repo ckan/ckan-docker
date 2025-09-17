3d2493e6e9a63bfcb5d1b34feb70c30033f85a29
bonnland <bonnland@ucar.edu>
2025-09-17 14:14:11 -0600

    Sync with master

--- ckan/docker-entrypoint.d/04_setup_wafs.sh
+++ ckan/docker-entrypoint.d/04_setup_wafs.sh
@@ -4,6 +4,7 @@
 if [[ $CKAN__PLUGINS == *"spatial"* ]]; then
 
   # Add API token to admin user, so we can create an Organization, thereby allowing creation of a WAF.
+  # NOTE: The application is not always ready to accept HTTP requests at this point, so we try a different way below.
 #  if [ ! -d "/tmp/apikey.txt" ]; then
 #      ckan -c ~/ckan.ini user token add ckan_admin api-token | tail -1 | xargs > /tmp/apikey.txt
 #      apiKey=`cat /tmp/apikey.txt`
@@ -11,7 +12,6 @@ if [[ $CKAN__PLUGINS == *"spatial"* ]]; then
 #  fi
 
   # Add API token to admin user, so we can create an Organization, thereby allowing creation of a WAF.
-  # NOTE: The application is not always ready for HTTP requests at this point, so we avoid using the action API.
   if [ ! -d "/tmp/apikey.txt" ]; then
       ckan -c ~/ckan.ini user token add ckan_admin api-token | tail -1 | xargs > /tmp/apikey.txt
       pip install ckanapi
@@ -21,8 +21,8 @@ if [[ $CKAN__PLUGINS == *"spatial"* ]]; then
   set -x
   whoami
 
-  ls -l /var/www/html
-
+  #rm -rf /var/www/html
+  ls -lR /var/www
 
   # Create web-accessible folder structure
   if [ ! -d "/var/www/html" ]; then
@@ -33,7 +33,6 @@ if [[ $CKAN__PLUGINS == *"spatial"* ]]; then
       cd /var/www/html && \
       git clone https://github.com/NCAR/sagedev-dset-harvest-test.git && \
       ckan -c ~/ckan.ini harvester source create "mini-waf3" "http://nginx:9000/sagedev-dset-harvest-test" "waf" "MINI WAF3" "TRUE" "ncar" "MANUAL" '{"user" : "ckan_admin", "read_only": true}'
-
       # We can't run this command right away, because nginx has not started up yet.
       #ckan -c ~/ckan.ini harvester run-test mini-waf
   fi

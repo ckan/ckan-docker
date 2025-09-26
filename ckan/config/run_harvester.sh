a87f17fa63bb472cee5e77111f44fd30d1000604
bonnland <bonnland@ucar.edu>
2025-09-26 12:21:15 -0600

    Change harvester logic to pull less often

--- ckan/config/run_harvester.sh
+++ ckan/config/run_harvester.sh
@@ -9,11 +9,12 @@ set -x
 WAF_FOLDER='/var/www/html/'
 
 ABORT_FAILED_JOBS='/usr/local/bin/ckan -c /srv/app/ckan.ini harvester abort-failed-jobs'
+MARK_JOBS_FINISHED='/usr/local/bin/ckan -c /srv/app/ckan.ini harvester run'
 
 
 # Try aborting any hung harvest jobs.  We need to resync with the DB after a DB server timeout.
-$ABORT_FAILED_JOBS
-
+#$ABORT_FAILED_JOBS
+$MARK_JOBS_FINISHED
 
 directories=$( ls -d $WAF_FOLDER*/ )
 
@@ -24,10 +25,10 @@ for directory in $directories; do
     unset changed
     /srv/app/check_git.sh | grep -q -v 'Up-to-date' && changed=1
 
-    # Update local git repository of metadata records.
-    git pull
 
+    # Update local git repository of metadata records.
     if [ -n "$changed" ]; then
+        git pull
 
         WAF_NAME=`basename $directory`
 

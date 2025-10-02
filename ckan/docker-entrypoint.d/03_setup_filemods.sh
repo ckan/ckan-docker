33021bb690b1d9ec879d1f19b4fbf957fd5d9c82
bonnland <bonnland@ucar.edu>
2025-10-02 13:49:06 -0600

    install library earlier

--- ckan/docker-entrypoint.d/03_setup_filemods.sh
+++ ckan/docker-entrypoint.d/03_setup_filemods.sh
@@ -20,11 +20,15 @@
      cd
   fi
 
+  # Provide library needed by Google Analytics plugin
+  if [[ $CKAN__PLUGINS == *"googleanalytics"* ]]; then
+       pip install oauth2client==4.1.3
+  fi
+
   # Initialize the harvester DB tables
   if [[ $CKAN__PLUGINS == *"harvest"* ]]; then
        ckan -c ~/ckan.ini db upgrade -p harvest
   fi
-
 
   # Use custom Solr schema for DASH Search
   # Link in our modified Solr schema file to be used for solr.
@@ -33,9 +37,5 @@
   ln -sf $SRC_DIR/ckanext-dsetsearch/solr/schema.solr-spatial-2.0.xml schema.xml
 
 
-  # Provide library needed by Google Analytics plugin
-  if [[ $CKAN__PLUGINS == *"googleanalytics"* ]]; then
-       pip install oauth2client==4.1.3
-  fi
 
   

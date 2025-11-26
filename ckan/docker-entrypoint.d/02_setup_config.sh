5ca2784553e5ce16814db285d09ff9ab29e3d0ff
bonnland <bonnland@ucar.edu>
2025-11-25 20:42:05 -0700

    Use OpenBao secret for repo pull

--- ckan/docker-entrypoint.d/02_setup_config.sh
+++ ckan/docker-entrypoint.d/02_setup_config.sh
@@ -6,7 +6,7 @@ if [[ $CKAN__PLUGINS == *"dsetsearch"* ]]; then
   set -x
 
 ### Install DASH Search custom plugin at runtime, to avoid baking in secrets
-  pip3 install -e "git+https://${REPO_TOKEN}@github.com/NCAR/ckanext-dsetsearch.git#egg=ckanext-dsetsearch" && \
+  pip3 install -e "git+https://${REPO_TOKEN}@github.com/NCAR/ckanext-dsetsearch.git#egg=ckanext-dsetsearch#egg=ckanext-dsetsearch" && \
   pip3 install -r /srv/app/src/ckanext-dsetsearch/pip-requirements.txt
 
   ckan config-tool --edit ./ckan.ini ckan.auth.public_user_details=false

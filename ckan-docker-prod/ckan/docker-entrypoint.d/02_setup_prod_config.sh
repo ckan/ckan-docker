d64f314658b25392ed8dc95cdcc76ceb6cbc07f3
Simon Ochotnicky <ochotnicky.simon1@gmail.com>
2026-06-01 13:39:14 +0200

    fix: write production CKAN config on startup

---
+++ ckan-docker-prod/ckan/docker-entrypoint.d/02_setup_prod_config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "Applying production CKAN configuration"
+
+ckan config-tool "$CKAN_INI" "ckan.site_url=${CKAN__SITE_URL:-$CKAN_SITE_URL}"
+ckan config-tool "$CKAN_INI" "ckan.locale_default=${CKAN__LOCALE_DEFAULT:-sk}"
+ckan config-tool "$CKAN_INI" "ckan.uploads_enabled=${CKAN__UPLOADS_ENABLED:-true}"
+ckan config-tool "$CKAN_INI" "ckan.storage_path=${CKAN__STORAGE_PATH:-$CKAN_STORAGE_PATH}"
+ckan config-tool "$CKAN_INI" "ckan.max_resource_size=${CKAN__MAX_RESOURCE_SIZE:-100}"
+ckan config-tool "$CKAN_INI" "ckan.max_image_size=${CKAN__MAX_IMAGE_SIZE:-10}"
+
+ckan config-tool "$CKAN_INI" "ckan.datapusher.url=${CKAN__DATAPUSHER__URL:-$CKAN_DATAPUSHER_URL}"
+ckan config-tool "$CKAN_INI" "ckan.datapusher.callback_url_base=${CKAN__DATAPUSHER__CALLBACK_URL_BASE:-http://ckan:5000}"
+ckan config-tool "$CKAN_INI" "ckan.datapusher.api_token=${CKAN__DATAPUSHER__API_TOKEN}"
+
+ckan config-tool "$CKAN_INI" "scheming.dataset_schemas=${CKAN___SCHEMING__DATASET_SCHEMAS:-ckanext.idsk:schemas/dcat_ap_sk.yaml}"
+ckan config-tool "$CKAN_INI" "scheming.presets=${CKAN___SCHEMING__PRESETS:-ckanext.scheming:presets.json ckanext.dcat.schemas:presets.yaml}"
+ckan config-tool "$CKAN_INI" "ckanext.dcat.rdf.profiles=${CKANEXT__DCAT__RDF__PROFILES:-euro_dcat_ap_2 euro_dcat_ap_scheming}"

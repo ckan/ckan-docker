032c797ad6a2fd9cfbbd4ef4c8ce686b2d8a733c
Simon Ochotnicky <ochotnicky.simon1@gmail.com>
2026-06-03 14:31:34 +0200

    Enable scheming profiles for IDSK

--- ckan/ckanext-idsk/setup.py
+++ ckan/ckanext-idsk/setup.py
@@ -1,11 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name='ckanext-idsk',
-    version='0.1',
+    name="ckanext-idsk",
+    version="0.1",
     packages=find_packages(),
-    entry_points='''
+    include_package_data=True,
+    package_data={
+        "ckanext.idsk": [
+            "schemas/*.yaml",
+            "templates/**/*.html",
+            "public/**/*",
+        ],
+    },
+    entry_points="""
         [ckan.plugins]
         idsk_theme=ckanext.idsk.plugin:IDSKThemePlugin
-    ''',
+    """,
 )
